6e35111885606bfff85cf1847ce5d89f7f3203ef
Jens van de Wiel <jens.vdwiel@gmail.com>
2025-10-09 15:55:19 +0200

    Initial commit

---
+++ ASPNETStarter.Server/Controllers/RoutePrefixConvention.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace ASPNETStarter.Server.Controllers;
+
+public class RoutePrefixConvention(string ns, string prefix) : IControllerModelConvention
+{
+    private readonly string _namespace = ns;
+    private readonly AttributeRouteModel _routePrefix = new(new RouteAttribute(prefix));
+
+    public void Apply(ControllerModel controller)
+    {
+        if (controller.ControllerType.Namespace == _namespace)
+            foreach (var selector in controller.Selectors)
+                if (selector.AttributeRouteModel != null)
+                    selector.AttributeRouteModel = AttributeRouteModel.CombineAttributeRouteModel(
+                        _routePrefix, selector.AttributeRouteModel);
+                else
+                    selector.AttributeRouteModel = _routePrefix;
+    }
+}
---
+++ ASPNETStarter.Server/Program.cs
@@ -0,0 +1,42 @@
+using ASPNETStarter.Server.Controllers;
+
+namespace ASPNETStarter.Server;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllers(options =>
+        {
+            options.Conventions.Add(new RoutePrefixConvention(
+                "ASPNETStarter.Server.Controllers.v1", "api/v1"));
+        });
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.MapFallbackToFile("/index.html");
+
+        app.Run();
+    }
+}
